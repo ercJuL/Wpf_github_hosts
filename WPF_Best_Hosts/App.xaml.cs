93be3cb31d668e014dd244ab44ae1e60a4ec647c
ercjul <erc@ercjul.com>
2019-06-19 10:21:14 +0800

    主界面可用

---
+++ WPF_Best_Hosts/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ShowMeTheXAML;
+
+namespace WPF_Best_Hosts
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            XamlDisplay.Init();
+            base.OnStartup(e);
+        }
+    }
+}
