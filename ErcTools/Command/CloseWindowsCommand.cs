3f16c036daa71ef075dd00c5f4b7cd7df1896632
ercjul <geofvt@gmail.com>
2019-12-10 21:57:42 +0800

    抽离关闭窗口方法

---
+++ ErcTools/Command/CloseWindowsCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace ErcTools.Command
+{
+    public class CloseWindowsCommand: ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        public void InvokeCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
--- ErcTools/ViewModel/MainViewModel.cs
+++ ErcTools/ViewModel/MainViewModel.cs
@@ -9,21 +9,10 @@ namespace ErcTools.ViewModel
 {
     public class MainViewModel: ViewModelBase
     {
-        private readonly DelegateCommand _closeWindowsCommand;
-        public ICommand CloseWindowsCommand => _closeWindowsCommand;
+        public ICommand CloseWindowsCommand { get; }
         public MainViewModel()
         {
-            _closeWindowsCommand = new DelegateCommand(OnCloseWindows, CanCloseWindows);
-        }
-
-        private void OnCloseWindows(object commandParameter)
-        {
-            Application.Current.Shutdown();
-        }
-
-        private bool CanCloseWindows(object commandParameter)
-        {
-            return true;
+            CloseWindowsCommand = new CloseWindowsCommand();
         }
     }
 }
